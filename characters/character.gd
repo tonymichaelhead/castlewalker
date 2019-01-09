edca623d6cdc1134e8c131b9c369838534f672d9
tonymichaelhead <tonymichaelhead@gmail.com>
2019-01-09 06:26:42 -0800

    Add walking animations to base character

--- characters/character.gd
+++ characters/character.gd
@@ -6,6 +6,7 @@ signal state_changed
 
 var input_direction = Vector2()
 var last_move_direction = Vector2(1, 0)
+var sprite_direction = "down"
 
 const MAX_WALK_SPEED = 400
 const MAX_RUN_SPEED = 700
@@ -26,6 +27,13 @@ func _physics_process(delta):
 	velocity = input_direction.normalized() * speed
 	move_and_slide(velocity)
 	
+	sprite_direction_loop()
+	
+	if input_direction != Vector2(0, 0):
+		animation_switch("walk")
+	else:
+		animation_switch("idle")
+	
 #	For reference, for handling collisions manually
 #	var motion = input_direction.normalized() * speed * delta
 #	move_and_collide(motion)
@@ -33,3 +41,19 @@ func _physics_process(delta):
 	# Instant movement
 	
 	# Two approaches to collisions
+	
+func sprite_direction_loop():
+	match input_direction:
+		Vector2(-1, 0):
+			sprite_direction = "left"
+		Vector2(1, 0):
+			sprite_direction = "right"
+		Vector2(0, -1):
+			sprite_direction = "up"
+		Vector2(0, 1):
+			sprite_direction = "down"
+
+func animation_switch(animation):
+	var new_animation = str(animation, "_", sprite_direction)
+	if $AnimationPlayer.current_animation != new_animation:
+		$AnimationPlayer.play(new_animation)
