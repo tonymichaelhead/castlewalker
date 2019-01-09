c285e9334df2417dea36df7624ffe9ca085d4dba
tonymichaelhead <tonymichaelhead@gmail.com>
2019-01-09 05:20:13 -0800

    Implement basic character movement and input maps

--- characters/character.gd
+++ characters/character.gd
@@ -1,9 +1,35 @@
 extends KinematicBody2D
 
+# For visualization (probably don't need)
+signal speed_updated
+signal state_changed
 
-func _ready():
-	pass
-	
+var input_direction = Vector2()
+var last_move_direction = Vector2(1, 0)
+
+const MAX_WALK_SPEED = 400
+const MAX_RUN_SPEED = 700
+
+var speed = 0.0
+var max_speed = 0.0
+
+var velocity = Vector2()
 	
 func _physics_process(delta):
-	pass
+	if input_direction:
+		last_move_direction = input_direction
+		if speed != max_speed:
+			speed = max_speed
+	else:
+		speed = 0
+		
+	velocity = input_direction.normalized() * speed
+	move_and_slide(velocity)
+	
+#	For reference, for handling collisions manually
+#	var motion = input_direction.normalized() * speed * delta
+#	move_and_collide(motion)
+	
+	# Instant movement
+	
+	# Two approaches to collisions
--- characters/player/player.gd
+++ characters/player/player.gd
@@ -1,5 +1,32 @@
 extends 'res://characters/character.gd'
 
+signal direction_changed
 
 func _physics_process(delta):
-	pass
+	input_direction = Vector2()
+	
+	# 4 directions (First block for reference)
+#	if Input.is_action_pressed('move_up'):
+#		input_direction.y = -1
+#	elif Input.is_action_pressed('move_down'):
+#		input_direction.y = 1
+#	if Input.is_action_pressed('move_right'):
+#		input_direction.x = 1
+#	if Input.is_action_pressed('move_left'):
+#		input_direction.x = -1
+	
+#	input_direction.x = float(Input.is_action_pressed('move_right')) - float(Input.is_action_pressed('move_left'))
+#	if not input_direction.x:
+#		input_direction.y = float(Input.is_action_pressed('move_down')) - float(Input.is_action_pressed('move_up'))
+	
+	# 8 directions
+	input_direction.x = float(Input.is_action_pressed('move_right')) - float(Input.is_action_pressed('move_left'))
+	input_direction.y = float(Input.is_action_pressed('move_down')) - float(Input.is_action_pressed('move_up'))
+	
+	
+	# For the DirectionVisualizer (Maybe won't use)
+	if input_direction and input_direction != last_move_direction:
+		emit_signal('direction', input_direction)
+		
+	# Run
+	max_speed = MAX_RUN_SPEED if Input.is_action_pressed('run') else MAX_WALK_SPEED
