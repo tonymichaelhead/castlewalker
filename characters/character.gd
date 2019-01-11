ac11bd59c5bab767c0ebde0a6bd188405c5a6e8e
tonymichaelhead <tonymichaelhead@gmail.com>
2019-01-10 19:37:21 -0800

    Implement state machine

--- characters/character.gd
+++ characters/character.gd
@@ -15,24 +15,45 @@ var speed = 0.0
 var max_speed = 0.0
 
 var velocity = Vector2()
+
+
+#Define character state
+enum STATE { IDLE, MOVE }
+var state = null	
+	
+func _ready():
+	_change_state(IDLE)
+	
+
+func _change_state(new_state):
+	emit_signal('state_changed', new_state) # Only for visualation markers, probably delete
+	
+	# Initialize new state
+	
+	match new_state:
+		IDLE:
+#			animation_switch("idle")
+			pass
+		MOVE:
+			pass
+	
+	state = new_state
+	
 	
 func _physics_process(delta):
-	if input_direction:
-		last_move_direction = input_direction
-		if speed != max_speed:
-			speed = max_speed
-	else:
-		speed = 0
+	update_direction()
+	
+	if state == IDLE and input_direction:
 		
-	velocity = input_direction.normalized() * speed
-	move_and_slide(velocity)
-	
-	sprite_direction_loop()
-	
-	if input_direction != Vector2(0, 0):
-		animation_switch("walk")
-	else:
-		animation_switch("idle")
+		_change_state(MOVE)
+	elif state == MOVE:
+#		if input_direction != Vector2(0, 0):
+		move()
+			
+#		else:
+#			_change_state(IDLE)
+		if speed == 0:
+			_change_state(IDLE)
 	
 #	For reference, for handling collisions manually
 #	var motion = input_direction.normalized() * speed * delta
@@ -41,6 +62,26 @@ func _physics_process(delta):
 	# Instant movement
 	
 	# Two approaches to collisions
+	
+func update_direction():
+	if input_direction:
+		last_move_direction = input_direction
+
+func move():
+	
+	if speed != max_speed:
+		speed = max_speed
+	else:
+		speed = 0
+	
+	velocity = input_direction.normalized() * speed
+	move_and_slide(velocity)
+	
+	sprite_direction_loop()
+	if input_direction != Vector2(0, 0):
+		animation_switch("walk")
+	else:
+		animation_switch("idle")
 	
 func sprite_direction_loop():
 	match input_direction:
