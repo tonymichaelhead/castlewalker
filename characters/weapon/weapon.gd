094119fe53ce6177da93986733651de57c82e673
tonymichaelhead <tonymichaelhead@gmail.com>
2019-01-23 19:04:21 -0800

    Implement sword combo

--- characters/weapon/weapon.gd
+++ characters/weapon/weapon.gd
@@ -5,7 +5,25 @@ signal attack_finished
 enum STATES { IDLE, ATTACK }
 var state = null
 
-var power = 2
+const MAX_COMBO_COUNT = 3
+var combo_count = 0
+
+var combo = [
+	{
+		'damage': 1,
+		'animation': 'attack_fast'
+	},
+	{
+		'damage': 1,
+		'animation': 'attack_fast'
+	},
+	{
+		'damage': 3,
+		'animation': 'attack_medium'
+	},
+]
+var attack_current = {}
+
 var hit_objects = []
 
 func _ready():
@@ -20,15 +38,18 @@ func _change_state(new_state):
 			hit_objects = []
 	match new_state:
 		IDLE:
-			monitoring = false
+			combo_count = 0
 			$AnimationPlayer.play('idle')
+			monitoring = false
 		ATTACK:
+			attack_current = combo[combo_count - 1]
+			$AnimationPlayer.play(attack_current['animation'])
 			monitoring = true
-			$AnimationPlayer.play('attack_straight')
 	state = new_state
 
 
 func attack():
+	combo_count += 1
 	_change_state(ATTACK)
 	
 
@@ -37,11 +58,15 @@ func _on_body_entered(body):
 	if body.get_rid().get_id() in hit_objects or body.is_a_parent_of(self):
 		return
 	hit_objects.append(body.get_rid().get_id())
-	body.take_damage(self, power)
+	body.take_damage(self, attack_current['damage'])
 
 
 func _on_AnimationPlayer_animation_finished(name):
 	if name == "idle":
 		return
-	_change_state(IDLE)
-	emit_signal("attack_finished")
+		
+	if combo_count < MAX_COMBO_COUNT:
+		attack()
+	else:
+		_change_state(IDLE)
+		emit_signal("attack_finished")
