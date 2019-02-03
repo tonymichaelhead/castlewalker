a109fb0d9bbfa6c2c117fc060615e237ebe5fc27
tonymichaelhead <tonymichaelhead@gmail.com>
2019-02-02 23:18:11 -0800

    Implement basic avoidance in skeletons

--- monsters/monster.gd
+++ monsters/monster.gd
@@ -3,13 +3,16 @@ extends KinematicBody2D
 enum STATES { IDLE, ROAM, RETURN, SPOT, FOLLOW, STAGGER, ATTACK, ATTACK_COOLDOWN, DIE, DEAD }
 var state = null
 
+var knockback_direction = Vector2()
+# TODO: Add other required methods and variables from dummy.gd
+
 var has_target = false
 var target_position = Vector2()
 
 var velocity = Vector2()
 const MASS = 1.4
 
-const FOLLOW_RANGE = 300.0
+const FOLLOW_RANGE = 400.0
 export(float) var max_follow_speed = 200.0
 export(float) var max_roam_speed = 125.0
 
@@ -21,6 +24,9 @@ var roam_target_position = Vector2()
 var roam_slow_radius = 0.0
 
 var spawn_position = Vector2()
+
+const AVOID_MAX_DISTANCE = 150.0
+const AVOID_FORCE = 260.0
 
 
 func _ready():
@@ -38,29 +44,52 @@ func _ready():
 	target_node.connect('died', self, '_on_target_died')
 	has_target = true
 
+func take_damage(source, amount):
+	if self.is_a_parent_of(source):
+		return
+	knockback_direction = (global_position - source.global_position).normalized()
+	$Health.take_damage(amount)
+	
 
 func follow(target_position, max_speed):
 	var desired_velocity = (target_position - position).normalized() * max_speed
-	var steering = (desired_velocity - velocity) / MASS
+	var push = calculate_avoid_force(desired_velocity)
+	var steering = (desired_velocity - velocity + push) / MASS
 	velocity += steering
 	return position.distance_to(target_position)
 
 
-func arrive_to(target_position, slow_radius, max_speed):
+func arrive_to(target_position, slow_radius, max_speed, avoid=false):
 	var desired_velocity = (target_position - position).normalized() * max_speed
 	var distance_to_target = position.distance_to(target_position)
 	if distance_to_target < slow_radius:
 		desired_velocity *= (distance_to_target / slow_radius) * 0.75 + 0.25
-		
-	var steering = (desired_velocity - velocity) / MASS
+	
+	var push = calculate_avoid_force(desired_velocity) if avoid else Vector2()
+	var steering = (desired_velocity - velocity + push) / MASS
 	velocity += steering
 	return distance_to_target
+
+
+func calculate_avoid_force(desired_velocity):
+	$RayCast2D.cast_to = desired_velocity.normalized() * AVOID_MAX_DISTANCE
+	$RayCast2D.force_raycast_update()
+	var push = Vector2()
+	if $RayCast2D.is_colliding():
+		var push_direction = desired_velocity.normalized().rotated(PI/2.0)
+		var point = $RayCast2D.get_collision_point()
+		push = push_direction * AVOID_FORCE * (1.0 - position.distance_to(point) / AVOID_MAX_DISTANCE)
+	return push
 
 
 func _change_state(new_state):
 	match state:
 		IDLE:
 			$Timer.stop()
+		RETURN:
+			$RayCast2D.visible = false
+		FOLLOW:
+			$RayCast2D.visible = false
 		
 	match new_state:
 		IDLE:
@@ -75,6 +104,10 @@ func _change_state(new_state):
 			var random_radius = randf() * ROAM_RADIUS * 0.5 + 0.5 * ROAM_RADIUS
 			roam_target_position = spawn_position + Vector2(cos(random_angle) * random_radius, sin(random_angle) * random_radius)
 			roam_slow_radius = spawn_position.distance_to(roam_target_position) / 2
+		RETURN:
+			$RayCast2D.visible = true
+		FOLLOW:
+			$RayCast2D.visible = true
 	state = new_state
 	
 	
@@ -97,13 +130,8 @@ func _physics_process(delta):
 				if not has_target:
 					return
 				_change_state(FOLLOW)
-		FOLLOW:
-			var distance_to_target = follow(target_position, max_follow_speed)
-			move_and_slide(velocity)
-			if distance_to_target > FOLLOW_RANGE:
-				_change_state(RETURN)
 		RETURN:
-			var distance_to_target = arrive_to(spawn_position, SLOW_RADIUS, max_roam_speed)
+			var distance_to_target = arrive_to(spawn_position, SLOW_RADIUS, max_roam_speed, true)
 			move_and_slide(velocity)
 			if distance_to_target < ARRIVE_DISTANCE:
 				_change_state(IDLE)
@@ -111,6 +139,12 @@ func _physics_process(delta):
 				if not has_target:
 					return
 				_change_state(FOLLOW)
+		FOLLOW:
+			var distance_to_target = follow(target_position, max_follow_speed)
+			move_and_slide(velocity)
+			if distance_to_target > FOLLOW_RANGE:
+				_change_state(RETURN)
+		
 
 func _on_target_position_changed(new_position):
 	target_position = new_position
