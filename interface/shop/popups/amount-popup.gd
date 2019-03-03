eb2beb6ba97aee981d266b7f8d9c0792e57446aa
tonymichaelhead <tonymichaelhead@gmail.com>
2019-03-03 14:58:20 -0800

    Accept slider amounts on button click

--- interface/shop/popups/amount-popup.gd
+++ interface/shop/popups/amount-popup.gd
@@ -4,6 +4,7 @@ signal amount_confirmed(value)
 
 onready var _amount_label = $VBoxContainer/Slider/Amount
 onready var _slider = $VBoxContainer/Slider/HSlider
+#onready var _accept_button = $VBoxContainer/OkButton TODO: probz delete
 
 
 func open():
@@ -24,7 +25,17 @@ func _input(event):
 	if event.is_action_pressed("ui_cancel"):
 		emit_signal("amount_confirmed", 0)
 		get_tree().set_input_as_handled()
+	# TODO: Do the same as ui accept, but checking if the ok button was clicked
 	if event.is_action_pressed("ui_accept"):
-		emit_signal("amount_confirmed", _slider.value)
-		get_tree().set_input_as_handled()
+		confirm_amount()
 	
+
+func confirm_amount():
+	print('amount confirmed')
+	emit_signal("amount_confirmed", _slider.value)
+	get_tree().set_input_as_handled()
+
+func _on_OkButton_pressed():
+	confirm_amount()
+
+
